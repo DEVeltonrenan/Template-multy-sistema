83f0a25c36bafddf3e2ebc8eb8bdb865134b226b
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2025-08-12 21:49:52 -0400

    fix: ajuste na biblioteca

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,22 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "./components/ui/accordion"
+
 
 export default function App() {
   return (
     <div className="min-h-screen bg-background">
-      testess
+      <Accordion type="single" collapsible>
+  <AccordionItem value="item-1">
+    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+    <AccordionContent>
+      Yes. It adheres to the WAI-ARIA design pattern.
+    </AccordionContent>
+  </AccordionItem>
+</Accordion>
     </div>
   );
 }
