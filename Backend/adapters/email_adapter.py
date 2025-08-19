d657a3642b99eee411159cdaf8a2bb205a85cb33
Elton Renan Macedo <renandrunline@gmail.com>
2025-08-18 23:27:54 -0400

    feat: adicionando implement de envio de email

--- Backend/adapters/email_adapter.py
+++ Backend/adapters/email_adapter.py
@@ -1,11 +1,26 @@
+import smtplib
+from email.mime.text import MIMEText
+# from email.mime.multipart import MIMEMultipart
+
 class EmailAdapter:
     def __init__(self, email: str):
         self.email = email
 
     def send_email(self, subject: str, body: str):
-        print(f"Sending email to {self.email} with subject {subject} and body {body}")
-        return True
-    
-    def send_email_with_attachment(self, subject: str, body: str, attachment: str):
-        print(f"Sending email to {self.email} with subject {subject} and body {body} and attachment {attachment}")
-        return True
+        sender_email = "renandrunline@gmail.com"
+        sender_password = "odyj pzcw nmvb kpid"  # Use an app password or environment variable for security
+        recipient_email = self.email
+
+        html_message = MIMEText(body, 'html')
+        html_message['Subject'] = subject
+        html_message['From'] = sender_email
+        html_message['To'] = recipient_email
+
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+            server.login(sender_email, sender_password)
+            server.sendmail(sender_email, recipient_email, html_message.as_string())
+
+    # def send_email_with_attachment(self, subject: str, body: str, attachment: str):
+    #     print(f"Sending email to {self.email} with subject {subject} and body {body} and attachment {attachment}")
+
+
--- Backend/main.py
+++ Backend/main.py
@@ -4,15 +4,17 @@ from fastapi import FastAPI
 from adapters.email_adapter import EmailAdapter
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(port=8000, host="0.0.0.0", reload=True)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-@app.post("/users")
-def create_user(userData: dict):
-    print(userData)
-    return {"user": userData}
+
+
+app = FastAPI()
+
+@app.post("/send-email")
+def send_email():
+    email = EmailAdapter("renandrunline@gmail.com", "odyj pzcw nmvb kpid")
+    sucesso = email.send_email(
+        recipient_email="renandrunline02@gmail.com",
+        subject="Teste de envio",
+        body="<h1>Olá!</h1><p>Email enviado via FastAPI + Python 🚀</p>"
+    )
+    return {"status": "enviado" if sucesso else "erro"}
+    
